13c70f36a60ac2d21d9887192d403e1e9ba7a8c8
Uri <oriol.castroarnau@gmail.com>
2019-01-29 22:04:03 +0100

    Add date field

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/0718f5ac-9a2e-44e6-a89f-6f97fc12cc60/deploy-status)](https://app.netlify.com/sites/pastanagapp-6by6january/deploys)
--- src/components/MyGame/assignPlayers.jsx
+++ src/components/MyGame/assignPlayers.jsx
@@ -2,15 +2,55 @@ import React, { Component } from 'react'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import axios from 'axios'
+import { Formik } from 'formik'
+
+import CustomDatePicker from '../SignUp/customDatePicker'
+
+const Form = props => {
+  const {
+    values: { separationDate },
+    handleSubmit,
+    handleChange,
+    setFieldTouched,
+    setFieldValue,
+  } = props
+  const change = (name, e) => {
+    e.persist()
+    handleChange(e)
+    setFieldTouched(name, true, false)
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <CustomDatePicker
+          name="separationDate"
+          value={separationDate}
+          onChange={setFieldValue}
+          onBlur={setFieldTouched}
+        />
+        <Button type="submit" variant="contained" color="primary" fullWidth>
+          Assigna
+        </Button>
+      </form>
+    </>
+  )
+}
 
 class AssignPlayers extends Component {
   constructor(props) {
     super(props)
+    this.state = { separationDate: '' }
   }
 
-  handleClick = () => {
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    })
+  }
+
+  submitForm = values => {
     const payload = {
-      breaking_date: '2003-02-25',
+      separationDate: values.separationDate,
     }
     axios
       .post('.netlify/functions/generateAndAssignKills', {
@@ -26,14 +66,34 @@ class AssignPlayers extends Component {
   }
 
   render() {
+    const values = {
+      separationDate: new Date('2003-02-25'),
+    }
     return (
       <div style={{ marginBottom: '40px' }}>
         <Typography variant="h6" paragraph>
           Assigna els emparellaments
         </Typography>
+        <Formik
+          render={props => <Form {...props} />}
+          initialValues={values}
+          onSubmit={this.submitForm}
+        />
+
+        {/* <TextField
+          id="separationDate"
+          name="separationDate"
+          helperText="Introdueix la data amb el format 1987-10-05"
+          value={this.state.separationDate}
+          onChange={this.handleChange('separationDate')}
+          label="Data de naixement per a fer la separació"
+          fullWidth
+          variant="outlined"
+          margin="normal"
+        />
         <Button variant="contained" color="primary" onClick={this.handleClick}>
           Assigna
-        </Button>
+        </Button> */}
       </div>
     )
   }
