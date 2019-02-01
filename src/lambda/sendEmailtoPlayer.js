c155bd43095cd94a1f34e0f560d777e6f22111f1
Uri <oriol.castroarnau@gmail.com>
2019-02-01 16:10:23 +0100

    Fix function

--- src/lambda/sendEmailtoPlayer.js
+++ src/lambda/sendEmailtoPlayer.js
@@ -18,31 +18,30 @@ exports.handler = async function(event) {
   const { email, name } = request.event.data.old
   console.log(email, name)
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-
-  const msg = {
-    to: `uri875@gmail.com`,
-    from: 'contacte@lapastanagadelrei.cat',
-    subject: 'Has estat eliminat del joc',
-    text: `Ens sap greu però has estat eliminat del joc de La Pastanaga del Rei.`,
-    html: `<p>Eliminat!</p>`,
+  try {
+    const msg = {
+      to: `uri875@gmail.com`,
+      from: 'contacte@lapastanagadelrei.cat',
+      subject: 'Has estat eliminat del joc',
+      text: `Ens sap greu però has estat eliminat del joc de La Pastanaga del Rei.`,
+      html: `<p>Eliminat!</p>`,
+    }
+    const resSgMail = await sgMail.send(msg)
+    console.log(resSgMail)
+  } catch (err) {
+    console.log(err.toString())
+    return {
+      statusCode: 500,
+      error: JSON.stringify(err),
+    }
   }
-  sgMail
-    .send(msg)
-    .then(() => {
-      console.log('Email send correctly')
-      const responseBody = {
-        status: 'Email send correctly to player',
-      }
-      return {
-        statusCode: 200,
-        body: JSON.stringify(responseBody),
-      }
-    })
-    .catch(err => {
-      console.log(error.toString())
-      return {
-        statusCode: 500,
-        error: JSON.stringify(err),
-      }
-    })
+
+  console.log('Email send correctly')
+  const responseBody = {
+    status: 'Email send correctly to player',
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(responseBody),
+  }
 }
