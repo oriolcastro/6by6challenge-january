43ec0297a9da107be4034fd29e15d4c8926f6639
Uri <oriol.castroarnau@gmail.com>
2019-02-11 17:54:57 +0100

    Add My Victims feature

---
+++ src/components/MyGame/myVictims.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Query } from 'react-apollo'
+import Typography from '@material-ui/core/Typography'
+
+import VictimsList from '../victimsList'
+import { GET_MY_KILLED_VICTIMS } from '../../utils/queries'
+
+const MyVictims = () => {
+  let player_id
+  if (typeof window !== 'undefined') {
+    player_id = localStorage.getItem('player_id')
+  }
+  return (
+    <div>
+      <Typography variant="h5" paragraph>
+        Llista de morts
+      </Typography>
+      <Query
+        query={GET_MY_KILLED_VICTIMS}
+        variables={{ player_id }}
+        pollInterval={3600000}
+        context={{ headers: { 'X-Hasura-Role': 'public' } }}
+        partialRefetch
+      >
+        {({ loading, error, data }) => {
+          if (loading) return null
+          if (error) return `Error: ${error}`
+          return (
+            <>
+              {!data.killsDev.length == 0 ? (
+                <VictimsList victims={data.killsDev} />
+              ) : (
+                <Typography variant="body2">
+                  Encara no has aconseguit cap víctima
+                </Typography>
+              )}
+            </>
+          )
+        }}
+      </Query>
+    </div>
+  )
+}
+
+export default MyVictims
---
+++ src/components/victimsList.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+
+const VictimsList = props => {
+  console.log(props.victims)
+  return (
+    <List>
+      {props.victims.map(({ victim }) => {
+        console.log(victim)
+        return (
+          <ListItem divider key={victim.player_id} disableGutters>
+            <ListItemText
+              primary={`${victim.name} ${victim.firstSurname} ${
+                victim.secondSurname
+              }`}
+            />
+            <ListItemText
+              secondary={victim.team.name}
+              secondaryTypographyProps={{ align: 'right' }}
+              style={{ maxWidth: '50%' }}
+            />
+          </ListItem>
+        )
+      })}
+    </List>
+  )
+}
+
+export default VictimsList
