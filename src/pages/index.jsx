9df1422364675731be7213ad23c134b97b46bff1
Uri <oriol.castroarnau@gmail.com>
2019-02-11 17:55:15 +0100

    Add Last Victims feature

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
+import { Query } from 'react-apollo'
 import withRoot from '../withRoot'
 
 import Layout from '../components/layout'
@@ -7,6 +8,9 @@ import SignUp from '../components/SignUp/index'
 import LandingHero from '../components/landingHero'
 import SEO from '../components/seo'
 import Countdown from '../components/countdown'
+
+import VictimsList from '../components/victimsList'
+import { GET_LAST_VICTIMS } from '../utils/queries'
 
 const IndexPage = () => (
   <Layout>
@@ -18,9 +22,39 @@ const IndexPage = () => (
     {/* <LandingHero />
     <SignUp /> */}
     <Typography variant="h5" paragraph>
-      Temps per l'inici del joc
+      Temps de joc restant
     </Typography>
-    <Countdown date="2019-02-24T00:00:00" />
+    <Countdown date="2019-03-06T20:00:00" />
+    <Typography variant="h5" paragraph>
+      Últimes víctimes
+    </Typography>
+    <Query
+      query={GET_LAST_VICTIMS}
+      pollInterval={3600000}
+      context={{
+        headers: {
+          'X-Hasura-Role': 'public',
+          'X-Hasura-Access-Key': process.env.GATSBY_HASURA_GRAPHQL_ACCESS_KEY,
+        },
+      }}
+      partialRefetch
+    >
+      {({ loading, error, data }) => {
+        if (loading) return null
+        if (error) return `Error: ${error}`
+        return (
+          <>
+            {!data.killsDev.length == 0 ? (
+              <VictimsList victims={data.killsDev} />
+            ) : (
+              <Typography variant="body2">
+                Encara no hi ha cap víctima
+              </Typography>
+            )}
+          </>
+        )
+      }}
+    </Query>
   </Layout>
 )
 
