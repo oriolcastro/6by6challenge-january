7ecec075601ebf91629b15ab2222e64ddddb3127
Uri <oriol.castroarnau@gmail.com>
2019-02-18 13:19:05 +0100

    Fix date error

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,7 +13,7 @@ const IndexPage = () => (
       <Typography variant="h3" align="center" paragraph>
         Falten{' '}
       </Typography>
-      <Countdown date="2019-02-28T00:0:01" />
+      <Countdown date="2019-02-28T00:00:01" />
       <Typography variant="h4" align="center" paragraph>
         per començar el joc
       </Typography>
