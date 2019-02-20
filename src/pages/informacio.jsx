c464a7aaa3a2b6dfde9ad8f0737bcd76db01137c
Uri <oriol.castroarnau@gmail.com>
2019-02-20 12:54:59 +0100

    Correct content

--- src/pages/informacio.jsx
+++ src/pages/informacio.jsx
@@ -21,21 +21,21 @@ const InfoPage = ({ data }) => (
       </Grid>
       <Grid item xl={6} lg={6} md={6} sm={12} xs={12}>
         <Typography variant="body2" align="justify" gutterBottom>
-          Benvingudes a la web de <strong>La Pastanaga del Rei</strong>, una nova eina per a fer
-          més dinàmic i atractiu el joc per excelència del Carnaval de Vilanova.
-          Podeu consultar de forma pública i en temps real el nombre de persones
-          que han estat eliminades avui i des de l'inici del joc, així com veure
-          qui son i en quina entitat participaven.
+        Benvingudes a la web de <strong>La Pastanaga del Rei</strong>, una nova eina per a fer
+més dinàmic i atractiu el joc per excel·lència del Carnaval de Vilanova.
+Podeu consultar de forma pública i en temps real el nombre de persones
+que han estat eliminades avui i des de l'inici del joc, així com veure
+qui són i en quina entitat participaven.
         </Typography>
         <Typography variant="body2" align="justify">
-          Les persones que us heu inscrit al joc també podeu accedir a la pàgina 
-           <strong> "El meu joc"</strong> i gaudir de experiencia amb que hem dissenyat la web. Per
-          accedir heu de fer servir el correu electrònic i contrasenya que vau
-          introduir al forumulari d'inscripció. Un cop dins podreu:
+        Les persones que us heu inscrit al joc també podeu accedir a la pàgina
+<strong> "El meu joc"</strong> i gaudir de l'experiència amb què hem dissenyat la web. Per
+accedir heu de fer servir el correu electrònic i contrasenya que vau
+introduir al formulari d'inscripció. Un cop dins podreu:
         </Typography>
         <Typography variant="body2" align="justify">
           <ul>
-            <li>Consultar les dades de la persona a la que heu de matar.</li>
+            <li>Consultar les dades de la persona a la qual heu de matar.</li>
             <li>Veure la llista de persones que ja heu eliminat.</li>
             <li>
               Indicar que heu eliminat a la vostra víctima o validar que us han
